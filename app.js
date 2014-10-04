853a39f9fe9045802238fe9daad8527e97c6c836
Kevin Washington <kevw12188@gmail.com>
2014-10-03 21:15:22 -0400

    shift of directives

--- app.js
+++ app.js
@@ -1,7 +1,40 @@
 var app = angular.module('app', []);
 
 
-app.directive('promiseExample', function(){
+
+app.directive('promiseExample1', function(){
+	return {
+		restrict: 'E', 
+		template: '<button class="btn btn-primary">Click this 1st</button>',
+		controller: function($scope, $q, $timeout){
+			
+			$scope.tester = function(){
+				//new deferred object
+				var deferred = $q.defer();
+
+				//deferred promise object
+				var promise = deferred.promise;
+
+				promise.then(function(data){
+					alert(data);
+				});
+
+				$timeout(function(){
+					deferred.resolve('I\'m from the very first button you clicked! - Look at the code if you dont believe me.');
+				}, 4500);
+			}
+
+		},
+		link: function(scope, element, attrs){
+			element.bind('click', function(){
+				scope.tester();
+			});
+		}
+	}
+})
+
+
+app.directive('promiseExample2', function(){
 	return {
 		restrict: 'E', 
 		template: '<button class="btn btn-primary pull-right">Then click me 2nd!</button>',
@@ -35,33 +68,3 @@ app.directive('promiseExample', function(){
 })
 
 
-app.directive('quickTest', function(){
-	return {
-		restrict: 'E', 
-		template: '<button class="btn btn-primary">Click this 1st</button>',
-		controller: function($scope, $q, $timeout){
-			
-			$scope.tester = function(){
-				//new deferred object
-				var deferred = $q.defer();
-
-				//deferred promise object
-				var promise = deferred.promise;
-
-				promise.then(function(data){
-					alert(data);
-				});
-
-				$timeout(function(){
-					deferred.resolve('I\'m from the very first button you clicked! - Look at the code if you dont believe me.');
-				}, 4500);
-			}
-
-		},
-		link: function(scope, element, attrs){
-			element.bind('click', function(){
-				scope.tester();
-			});
-		}
-	}
-})
